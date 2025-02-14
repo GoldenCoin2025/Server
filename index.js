75355ab230979933d645cbf6bc62282334da6c4a
GoldCoin <conectados0000@gmail.com>
2025-02-14 20:11:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const WebSocket = require('ws');
-
-// Configuración del puerto (utilizamos el puerto dinámico de Railway)
-const port = process.env.PORT || 8080; // Si Railway no asigna un puerto, usará el 8080 por defecto
-
-// Crear el servidor WebSocket
-const wss = new WebSocket.Server({ port });
-
-// Cuando un cliente se conecta
-wss.on('connection', ws => {
-  console.log('Nuevo cliente conectado');
-
-  // Enviar un mensaje al cliente
-  ws.send('¡Conexión exitosa!');
-
+.log('Nuevo cliente conectado');  // Enviar un mensaje al cliente  ws.send('¡Conexión exitosa!');
   // Escuchar los mensajes recibidos del cliente
   ws.on('message', message => {
     console.log(`Mensaje recibido: ${message}`);
